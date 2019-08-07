f732d9570c53f33ea510b089f55ecba42cd0f67d
Ben Walker <benwalker92@me.com>
2019-08-07 18:06:54 +0100

    Set thickness of all lines and tweak alpha of white lines

--- AlignmentGrid/AlignmentGrid.lua
+++ AlignmentGrid/AlignmentGrid.lua
@@ -9,22 +9,30 @@ height = GetScreenHeight() / numberOfHorizontalLines
 
 for i = 1, (numberOfVerticalLines - 1) do
 	local line = frame:CreateLine()
+	line:SetThickness(1)
+
+	-- Sets the color of the line based on if it is in the center of the screen
 	if i == (numberOfVerticalLines / 2) then
 		line:SetColorTexture(1, 1, 0, 0.5)
 	else
-		line:SetColorTexture(1, 1, 1, 0.15)
+		line:SetColorTexture(1, 1, 1, 0.25)
 	end
+
 	line:SetStartPoint('TOPLEFT', i * width, 0)
 	line:SetEndPoint('BOTTOMLEFT', i * width, 0)
 end
 
 for i = 1, (numberOfHorizontalLines - 1) do
 	local line = frame:CreateLine()
+	line:SetThickness(1)
+
+	-- Sets the color of the line based on if it is in the center of the screen
 	if i == (numberOfHorizontalLines / 2) then
 		line:SetColorTexture(1, 1, 0, 0.5)
 	else
-		line:SetColorTexture(1, 1, 1, 0.15)
+		line:SetColorTexture(1, 1, 1, 0.25)
 	end
+
 	line:SetStartPoint('BOTTOMLEFT', 0, i * height)
 	line:SetEndPoint('BOTTOMRIGHT', 0, i * height)
 end
