f7575e8811c81fd5efeb80bd563e78900ed2fd08
Ben Walker <benwalker92@me.com>
2019-08-07 17:50:44 +0100

    Change line variable instantiation from CreateTexture() to CreateLine()
    Line makes more sense here and has a more understandable/readable API.

--- AlignmentGrid/AlignmentGrid.lua
+++ AlignmentGrid/AlignmentGrid.lua
@@ -7,26 +7,26 @@ numberOfHorizontalLines = 36
 width = GetScreenWidth() / numberOfVerticalLines
 height = GetScreenHeight() / numberOfHorizontalLines
 
-	local line = frame:CreateTexture(nil, 'BACKGROUND')
 for i = 0, numberOfVerticalLines do
+	local line = frame:CreateLine()
 	if i == (numberOfVerticalLines / 2) then
 		line:SetColorTexture(1, 1, 0, 0.5)
 	else
 		line:SetColorTexture(1, 1, 1, 0.15)
 	end
-	line:SetPoint('TOPLEFT', frame, 'TOPLEFT', i * width - 1, 0)
-	line:SetPoint('BOTTOMRIGHT', frame, 'BOTTOMLEFT', i * width + 1, 0)
+	line:SetStartPoint('TOPLEFT', i * width, 0)
+	line:SetEndPoint('BOTTOMLEFT', i * width, 0)
 end
 
 for i = 0, numberOfHorizontalLines do
-	local line = frame:CreateTexture(nil, 'BACKGROUND')
+	local line = frame:CreateLine()
 	if i == (numberOfHorizontalLines / 2) then
 		line:SetColorTexture(1, 1, 0, 0.5)
 	else
 		line:SetColorTexture(1, 1, 1, 0.15)
 	end
-	line:SetPoint('TOPLEFT', frame, 'TOPLEFT', 0, -i * height + 1)
-	line:SetPoint('BOTTOMRIGHT', frame, 'TOPRIGHT', 0, -i * height - 1)
+	line:SetStartPoint('BOTTOMLEFT', 0, i * height)
+	line:SetEndPoint('BOTTOMRIGHT', 0, i * height)
 end
 
 frame:Show()
